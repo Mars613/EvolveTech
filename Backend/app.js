42e803bc0191c9da1c32cc8531701d151db27d78
Mars613 <rodrigues.marcia1203@gmail.com>
2024-09-28 02:10:18 +0530

    backend2

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 // import { dbConnection } from "./database/dbConnection.js";
-// import { config } from "dotenv";
+import { config } from "dotenv";
 // import cookieParser from "cookie-parser";
-// import cors from "cors";
+import cors from "cors";
 // import fileUpload from "express-fileupload";
 // import { errorMiddleware } from "./middlewares/error.js";
 // import messageRouter from "./router/messageRouter.js";
@@ -10,15 +10,15 @@ import express from "express";
 // import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
-// config({ path: "./config.env" });
+config({ path: "./config/config.env" });
 
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
-//     method: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+        method: ["GET", "POST", "DELETE", "PUT"],
+        credentials: true,
+    })
+);
 
 // app.use(cookieParser());
 // app.use(express.json());
