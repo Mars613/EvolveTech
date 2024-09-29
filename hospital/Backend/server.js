238834e2bad11dcf9694604c6d2268ee6446fe7e
Mars613 <rodrigues.marcia1203@gmail.com>
2024-09-29 15:52:35 +0530

    backend16

--- hospital/Backend/server.js
+++ hospital/Backend/server.js
@@ -1,7 +1,5 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
-import http from "http"; // Import http module
-import socketIo from "socket.io"; // Import socket.io
 
 // Cloudinary configuration
 cloudinary.v2.config({
@@ -10,29 +8,9 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = http.createServer(app); // Create an HTTP server
-const io = socketIo(server); // Attach Socket.IO to the server
-
-// Socket.IO connection
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('joinRoom', (room) => {
-    socket.join(room);
-    console.log(`User joined room: ${room}`);
-  });
-
-  socket.on('message', (data) => {
-    io.to(data.room).emit('message', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
+console.log("Starting server...");
 
 // Start the server
-console.log('Starting server...');
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server listening at port ${process.env.PORT}`);
 });
