6f8ee4e0c564e04bab3a347c5f9bcd2f71a6394d
EshaTuscano <eshatuscano@yahoo.com>
2024-09-29 08:24:55 +0530

    appointmentform

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
     <>
       <section className="container form-component">
         <img src="/logo.png" alt="logo" className="logo"  style={{ width: "500px", height: "auto" }}  />
-        <h1 className="form-title">WELCOME TO VITALHUB</h1>
+        <h1 className="form-title">WELCOME TO VITAL HUB</h1>
         <p>Only Admins Are Allowed To Access These Resources!</p>
         <form onSubmit={handleLogin}>
           <input
