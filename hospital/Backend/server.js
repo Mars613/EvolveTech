b9edccd288ddcb2f6868d9797b8291652331b03d
EshaTuscano <eshatuscano@yahoo.com>
2024-09-29 15:50:15 +0530

    server

--- hospital/Backend/server.js
+++ hospital/Backend/server.js
@@ -1,38 +1,13 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
-import http from "http"; // Import http module
-import socketIo from "socket.io"; // Import socket.io
 
-// Cloudinary configuration
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = http.createServer(app); // Create an HTTP server
-const io = socketIo(server); // Attach Socket.IO to the server
-
-// Socket.IO connection
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('joinRoom', (room) => {
-    socket.join(room);
-    console.log(`User joined room: ${room}`);
-  });
-
-  socket.on('message', (data) => {
-    io.to(data.room).emit('message', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
-// Start the server
 console.log('Starting server...');
-server.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(Server listening at port ${process.env.PORT});
 });
